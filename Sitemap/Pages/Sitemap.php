f96eae77b00026170512a95f8415e06ce2daaeef
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-08 18:36:36 +0100

    Load and store

--- Sitemap/Pages/Sitemap.php
+++ Sitemap/Pages/Sitemap.php
@@ -12,6 +12,16 @@ namespace IdnoPlugins\Sitemap\Pages {
 	 */
 	protected function load() {
 	    
+	    // We have a caching interface
+	    if (\Idno\Core\site()->cache) {
+		return \Idno\Core\site()->cache()->load('sitemap.xml');
+	    }
+	    
+	    // No caching interface, use generic data item.
+	    $sitemaps = \Idno\Entities\GenericDataItem::getByDatatype('Sitemap/Sitemap');
+	    $sitemap = $sitemaps[0];
+	    
+	    return $sitemap->xml;
 	}
 	
 	
@@ -21,6 +31,22 @@ namespace IdnoPlugins\Sitemap\Pages {
 	 */
 	protected function store($xml) {
 	    
+	    // We have a caching interface
+	    if (\Idno\Core\site()->cache) {
+		return \Idno\Core\site()->cache()->store('sitemap.xml', $xml);
+	    }
+	    
+	    // No caching interface, use generic data item.
+	    if ($sitemaps = \Idno\Entities\GenericDataItem::getByDatatype('Sitemap/Sitemap'))
+		$sitemap = $sitemaps[0];
+	    if (!$sitemap) {
+		$sitemap = new \Idno\Entities\GenericDataItem();
+		$sitemap->setDatatype('Sitemap/Sitemap');
+	    }
+	    
+	    $sitemap->xml = $xml;
+	    
+	    return $sitemap->save();
 	}
 	
 	function getContent() {
