783b1ad134ec61c22c728d8f01d2821814a02e78
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-08 19:55:15 +0100

    Store schema around xml, so we don't need to update complete list.

--- Sitemap/Pages/Sitemap.php
+++ Sitemap/Pages/Sitemap.php
@@ -22,7 +22,11 @@ namespace IdnoPlugins\Sitemap\Pages {
                 header("Cache-Control: public");
 		header("Content-Type: application/xml");
 		
+		
+		echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+		echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
 		echo $xml;
+		echo "</urlset>";
 	    }
 	    else 
 	    {
