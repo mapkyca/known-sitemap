bd2ae75b76720f69793f6eb49c0227a0263cb5c6
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-08 19:51:42 +0100

    Moving functions

--- Sitemap/Pages/Sitemap.php
+++ Sitemap/Pages/Sitemap.php
@@ -5,53 +5,13 @@ namespace IdnoPlugins\Sitemap\Pages {
     /**
      * Default class to serve the Minds callback
      */
-    class Callback extends \Idno\Common\Page {
+    class Sitemap extends \Idno\Common\Page {
 
-	/**
-	 * Attempt to find a stored XML file.
-	 */
-	protected function load() {
-	    
-	    // We have a caching interface
-	    if (\Idno\Core\site()->cache) {
-		return \Idno\Core\site()->cache()->load('sitemap.xml');
-	    }
-	    
-	    // No caching interface, use generic data item.
-	    $sitemaps = \Idno\Entities\GenericDataItem::getByDatatype('Sitemap/Sitemap');
-	    $sitemap = $sitemaps[0];
-	    
-	    return $sitemap->xml;
-	}
 	
-	
-	/**
-	 * Save a raw XML file.
-	 * @param string $xml Raw xml
-	 */
-	protected function store($xml) {
-	    
-	    // We have a caching interface
-	    if (\Idno\Core\site()->cache) {
-		return \Idno\Core\site()->cache()->store('sitemap.xml', $xml);
-	    }
-	    
-	    // No caching interface, use generic data item.
-	    if ($sitemaps = \Idno\Entities\GenericDataItem::getByDatatype('Sitemap/Sitemap'))
-		$sitemap = $sitemaps[0];
-	    if (!$sitemap) {
-		$sitemap = new \Idno\Entities\GenericDataItem();
-		$sitemap->setDatatype('Sitemap/Sitemap');
-	    }
-	    
-	    $sitemap->xml = $xml;
-	    
-	    return $sitemap->save();
-	}
 	
 	function getContent() {
 	    
-	    $xml = $this->load();
+	    $xml = \IdnoPlugins\Sitemap\Main::load();
 	    
 	    if (!$xml)
 		$xml = \IdnoPlugins\Sitemap\Main::generate ();
